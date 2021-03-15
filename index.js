6df1c8d06b11811f75b5ca61e0039642f57bc9c7
pj <pjhg14@gmail.com>
2021-03-15 14:54:47 -0400

    commit tree e575ee57e02c1607d94fa1f00a08a7423a6ee9bb

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 9;
+const num4 = 5;
+
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,14,9,6,10,20,5,16,19,7);
